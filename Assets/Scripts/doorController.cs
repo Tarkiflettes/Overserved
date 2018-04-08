8a8e53ed12f0f20505add6d413405af1071fb1a0
Alexis Poupelin <codevirtuel@gmail.com>
2018-04-08 11:11:07 +0200

    Added launchAnimation() function to door prefab

---
+++ Assets/Scripts/doorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        launchAnimation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void launchAnimation() {
+        GetComponent<Animation>().Play();
+    }
+}
